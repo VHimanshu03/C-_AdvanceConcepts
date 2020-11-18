c0b30bb3b6b277dc717a4c12b3f189fb03b9fb25
VHimanshu03 <30958998+VHimanshu03@users.noreply.github.com>
2020-11-18 09:43:19 +0530

    Add files via upload
    Async and Await in C_Sharp.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method();
+            Console.WriteLine("Main Thread");
+            Console.ReadLine();
+        }
+
+        public static async void Method()
+        {
+            await Task.Run(new Action(LongTask));
+            Console.WriteLine("New Thread");  //waits until LongTask is finished.
+        }
+
+        public static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+    }
+    class Test
+    {
+
+    }
+}
